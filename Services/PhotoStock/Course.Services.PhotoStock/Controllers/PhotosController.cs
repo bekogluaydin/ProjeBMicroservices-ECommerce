2494285f7ab55e34694fdf6cf27289968d442b64
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-03-30 12:36:58 +0300

    photos controller - PhotoDelete metodu yazıldı.

--- Services/PhotoStock/Course.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/Course.Services.PhotoStock/Controllers/PhotosController.cs
@@ -36,5 +36,16 @@ namespace Course.Services.PhotoStock.Controllers
 
             return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
         }
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if (!System.IO.File.Exists(path)) return CreateActionResultInstance(Response<NoContent>.Fail("photo is not found", 404));
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
     }
 }
