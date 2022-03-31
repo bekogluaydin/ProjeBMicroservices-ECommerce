a3e0b706e469cc219281967395424b5c7a870637
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-03-31 11:50:31 +0300

    ISharedIdentityService ve SharedIdentityService oluşturuldu. IdentityServer'da kullanıcı Token aldığında içinde sub olarak bir tpye/key - value değeri bulunur. Bu sub sayesinde kullanıcının tüm bilgilerine ulaşulabilir. Her serviste (Basket, Register, Login vb.) sürekli Token içindeki sub'a ulaşmak için kod yazmak yerine ISharedIdentityService ve SharedIdentityService oluşturuldu.

--- Services/Basket/Course.Services.Basket/Startup.cs
+++ Services/Basket/Course.Services.Basket/Startup.cs
@@ -1,5 +1,6 @@
 using Course.Services.Basket.Services;
 using Course.Services.Basket.Settings;
+using Course.Shared.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,8 @@ namespace Course.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
 
             services.AddSingleton<RedisService>(sp =>
