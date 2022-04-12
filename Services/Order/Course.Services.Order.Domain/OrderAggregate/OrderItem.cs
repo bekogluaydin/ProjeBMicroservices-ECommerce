a955d2cf0144d18282c3000bf4721bb66fd0c19f
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-04-12 13:53:34 +0300

    Order, OrderItem ve Adress sınıfları oluşturuldu. İlgili kodlar yazıldıç

---
+++ Services/Order/Course.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem:Entity
+    {
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureURL { get; private set; }
+        public Decimal Price { get; private set; }
+
+        public OrderItem(string productId, string productName, string pictureURL, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureURL = pictureURL;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureURL = pictureUrl;
+            Price = price;
+        }
+    }
+}
