b35b483860cd3dbb15cc8939191a9922eb973bb7
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-04-13 16:06:21 +0300

    MediatR-Command-CommandHandler oluşturuldu

---
+++ Services/Order/Course.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Course.Services.Order.Application.Commands;
+using Course.Services.Order.Application.Dtos;
+using Course.Services.Order.Domain.OrderAggregate;
+using Course.Services.Order.Infrastructure;
+using Course.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _contex;
+
+        public CreateOrderCommandHandler(OrderDbContext contex)
+        {
+            _contex = contex;
+        }
+
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAdress = new Adress(request.AdressDto.Province, request.AdressDto.District, request.AdressDto.Street, request.AdressDto.ZipCode, request.AdressDto.Line);
+
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyerID, newAdress);
+
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureURL);
+            });
+
+           await _contex.Orders.AddAsync(newOrder);
+
+           await _contex.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id }, 200);
+        }
+    }
+}
