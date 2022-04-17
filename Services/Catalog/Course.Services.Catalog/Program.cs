9564fd3bdb33aac2cc3253632afd0e739e2bcfc9
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-04-17 19:47:24 +0300

    Uygulama başlangıcında eğer Kategori boş ise 2 tane örnek kategori oluşturacak kod yazıldı

--- Services/Catalog/Course.Services.Catalog/Program.cs
+++ Services/Catalog/Course.Services.Catalog/Program.cs
@@ -1,5 +1,8 @@
+using Course.Services.Catalog.Dtos;
+using Course.Services.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,20 @@ namespace Course.Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          var host =  CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+
+                if (!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryCreateDto { Name = "Asp.net Core Kursu" }).Wait();
+                    categoryService.CreateAsync(new CategoryCreateDto { Name = "Asp.net Core API Kursu" }).Wait();
+                }
+            }  
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
