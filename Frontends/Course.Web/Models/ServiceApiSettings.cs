6b2cd7fd21ce8d5b1d1d3ecdb050560c2bc3bb6e
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-04-22 10:24:19 +0300

    ServiceApiSettings oluşturuldu gerekli kodlar yazıldı

---
+++ Frontends/Course.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string PhotoStockUrl { get; set; }
+    }
+}
