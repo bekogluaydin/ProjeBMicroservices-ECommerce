e49ab43203e9ddeb45221c8b5ac45645cf564275
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-04-29 12:29:51 +0300

    UserViewModel, IUserService interface ve onu implement eden UserService class oluşturulması ve kodlanması.

--- Frontends/Course.Web/Services/UserService.cs
+++ Frontends/Course.Web/Services/UserService.cs
@@ -3,15 +3,24 @@ using Course.Web.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace Course.Web.Services
 {
     public class UserService : IUserService
     {
-        public Task<UserViewModel> GetUser()
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserViewModel> GetUser()
+        {
+            return await _httpClient.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
         }
     }
 }
