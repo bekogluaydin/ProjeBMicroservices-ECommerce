1f6453c4736637a6efdaa5156415578a723bd6f7
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-05-04 13:59:31 +0300

    1-) BasketViewModel: Sepetin kendisini tutacak viewmodel. “BasketItemViewModel” liste olarak içinde barındıracak. İndirim kodu, indirim oranı tutacak. 2-) BasketItemViewModel: sepetteki ürünlerin ismini, fiyatını, adetini vb. tutacak viewmodel

---
+++ Frontends/Course.Web/Models/Baskets/BasketViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Web.Models.Baskets
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+
+        public string DiscountCode { get; set; }
+
+        public int? DiscountRate { get; set; }
+
+        private List<BasketItemViewModel> _basketItems { get; set; }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (hasDiscount)
+                {
+                    // Örnek; kurs fiyat 100 TL indirim kodu %10
+                    _basketItems.ForEach(x =>
+                    {
+                    var discountPrice = x.Price * ((decimal)DiscountRate.Value / 100); // buradan 10 TL gelir.
+                        x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2)); // kurs fiyat 100 tl - indirim 10 TL = 90,00 TL
+                    });
+                }
+                return _basketItems;
+            }
+            set { _basketItems = value; }
+        }
+
+        public decimal TotalPrice
+        {
+            get => _basketItems.Sum(x => x.GetCurrentPrice * x.Quantity);
+        }
+
+        public bool hasDiscount
+        {
+            get => !string.IsNullOrEmpty(DiscountCode);
+        }
+    }
+}
