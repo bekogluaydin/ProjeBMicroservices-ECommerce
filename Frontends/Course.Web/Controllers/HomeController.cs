28aa14296902e6f28ac2ddaa969bc39b9de039fd
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-05-04 13:20:29 +0300

    148. UnAuthorizeException hatası için handler yazıldı.

--- Frontends/Course.Web/Controllers/HomeController.cs
+++ Frontends/Course.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Course.Web;
+using Course.Web.Exceptions;
 using Course.Web.Models;
 using Course.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -35,6 +37,13 @@ namespace Course.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (errorFeature!=null && errorFeature.Error is UnAuthorizeException)
+            {
+                return RedirectToAction(nameof(AuthController.Logout), "Auth");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
