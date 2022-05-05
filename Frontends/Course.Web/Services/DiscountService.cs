ff7e68ee266662f960f84ea8c1a513066314571b
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-05-05 13:45:37 +0300

    160. IDiscountService-DiscountService

---
+++ Frontends/Course.Web/Services/DiscountService.cs
@@ -0,0 +1,17 @@
+﻿using Course.Web.Models.Discounts;
+using Course.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Web.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
