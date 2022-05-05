b332feb3eb6358c5e298dd07ad7c569a6cf16f6f
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-05-05 13:50:19 +0300

    161. DiscountService method

--- Frontends/Course.Web/Services/DiscountService.cs
+++ Frontends/Course.Web/Services/DiscountService.cs
@@ -1,17 +1,38 @@
-﻿using Course.Web.Models.Discounts;
+﻿using Course.Shared.Dtos;
+using Course.Web.Models.Discounts;
 using Course.Web.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace Course.Web.Services
 {
     public class DiscountService : IDiscountService
     {
-        public Task<DiscountViewModel> GetDiscount(string discountCode)
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            // [controller]/[action]/{code}
+
+            var response = await _httpClient.GetAsync($"discounts/GetByCode/{discountCode}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var discount = await response.Content.ReadFromJsonAsync<Response<DiscountViewModel>>();
+
+            return discount.Data;
         }
     }
 }
