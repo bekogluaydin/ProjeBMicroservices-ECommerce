e372817078e6951a71384304d9be3e03d8fbebd4
Aydın BEKOĞLU <aydinbek97@gmail.com>
2022-05-18 18:55:25 +0300

    .

---
+++ IdentityServer/Course.IdentityServer/Services/TokenExchangeExtensionGrantValidator.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.IdentityServer.Services
+{
+    public class TokenExchangeExtensionGrantValidator : IExtensionGrantValidator
+    {
+        public string GrantType => "urn:ietf:params:oauth:grant-type:token-exchange";
+
+        public Task ValidateAsync(ExtensionGrantValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
